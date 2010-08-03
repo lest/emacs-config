c148e6e559ffade9967da2bcdc2cdc1a6b4b1d6e
lest <just.lest@gmail.com>
2010-08-03 22:39:46 +0300

    ruby-mode for .irbrc and .caprc

--- rc-ruby.el
+++ rc-ruby.el
@@ -9,6 +9,7 @@
 (add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Capfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("/\\.\\(irb\\|cap\\)rc$" . ruby-mode))
 
 ;; flymake
 (eval-after-load 'ruby-mode
