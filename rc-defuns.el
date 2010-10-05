fffee00ecc865cb7dfe2446bc3c6245cf25ef415
yas <ilyukevich.victor@gmail.com>
2010-10-05 13:27:36 +0300

    add using of ack

--- rc-defuns.el
+++ rc-defuns.el
@@ -70,3 +70,22 @@ Symbols matching the text at point are put first in the completion list."
   (let ((file (ido-completing-read "Choose recent file: " recentf-list nil t)))
     (when file
       (find-file file))))
+
+(defvar ack-command "ack --nogroup --nocolor ")
+(defvar ack-history nil)
+(defvar ack-host-defaults-alist nil)
+(defun ack ()
+  "Like grep, but using ack-command as the default"
+  (interactive)
+  ;; Make sure grep has been initialized
+  (if (>= emacs-major-version 22)
+      (require 'grep)
+    (require 'compile))
+  ;; Close STDIN to keep ack from going into filter mode
+  (let ((null-device (format "< %s" null-device))
+        (grep-command ack-command)
+        (grep-history ack-history)
+        (grep-host-defaults-alist ack-host-defaults-alist))
+    (call-interactively 'grep)
+    (setq ack-history             grep-history
+          ack-host-defaults-alist grep-host-defaults-alist)))
