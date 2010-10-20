66e4c8ee5b29072848c0032b43c8b0b738fe1146
lest <just.lest@gmail.com>
2010-10-20 09:55:43 +0300

    remove org-mode from vendor

--- rc-org.el
+++ rc-org.el
@@ -1,10 +1,2 @@
-(setq load-path (cons (concat config-dir "vendor/org-mode/lisp") load-path))
-(setq load-path (cons (concat config-dir "vendor/org-mode/contrib/lisp") load-path))
-(require 'org-install)
-
-(add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
-
 (setq org-special-ctrl-k t
       org-completion-use-ido t)
-
-(require 'org-collector)
