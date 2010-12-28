e5f16ac1d8af012cf087fb1e020dd7d858ad1ca0
lest <just.lest@gmail.com>
2010-12-28 11:53:01 +0200

    update erlang config

--- rc-erlang.el
+++ rc-erlang.el
@@ -1,20 +1,7 @@
-;; erlang-mode
 (add-to-list 'load-path (concat config-dir "vendor/erlang-mode"))
 (require 'erlang-start)
 
-(require 'flymake)
-
-(defun flymake-erlang-init ()
-  (let* ((temp-file (flymake-init-create-temp-buffer-copy 'flymake-create-temp-inplace))
-         (local-file (file-relative-name temp-file
-                                         (file-name-directory buffer-file-name))))
-    (list (concat config-dir "bin/eflymake") (list local-file))))
-
-(add-to-list 'flymake-allowed-file-name-masks
-	     '("\\.erl\\'" flymake-erlang-init))
-
-(defun my-erlang-mode-hook ()
-  (flymake-mode 1))
-(add-hook 'erlang-mode-hook 'my-erlang-mode-hook)
+(require 'erlang-flymake)
+(erlang-flymake-only-on-save)
 
 (add-to-list 'auto-mode-alist '("rebar\\.config$" . erlang-mode))
