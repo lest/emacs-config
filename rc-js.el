0067cb15752ff4d0b72922feac697e1dfbcb00cb
lest <just.lest@gmail.com>
2010-12-28 11:21:38 +0200

    remove espresso mode as emacs 23.2 includes it named js-mode

--- rc-js.el
+++ rc-js.el
@@ -1,7 +1,4 @@
-(autoload 'espresso-mode "espresso" "Start espresso-mode" t)
+(add-to-list 'auto-mode-alist '("\\.json$" . js-mode))
+(add-to-list 'auto-mode-alist '("\\.js\\.erb$" . js-mode))
 
-(add-to-list 'auto-mode-alist '("\\.js$" . espresso-mode))
-(add-to-list 'auto-mode-alist '("\\.json$" . espresso-mode))
-(add-to-list 'auto-mode-alist '("\\.js\\.erb$" . espresso-mode))
-
-(setq espresso-indent-level 2)
+(setq js-indent-level 2)
