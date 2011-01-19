3387ad6ed3beeb83dcfb4d64962f2b9c3e90dfb5
lest <just.lest@gmail.com>
2011-01-19 17:46:15 +0200

    refactor haskell mode hook

--- rc-haskell.el
+++ rc-haskell.el
@@ -1,12 +1,10 @@
 (add-to-list 'load-path (concat config-dir "vendor/haskell-mode"))
 (load (concat config-dir "vendor/haskell-mode/haskell-site-file"))
 
-(defun lest/haskell-mode-hook ()
+(defun my-haskell-mode-hook ()
   (turn-on-haskell-doc-mode)
-  (turn-on-haskell-indent)
-  ;; (turn-on-haskell-indentation)
-  (setq haskell-font-lock-symbols t))
+  (turn-on-haskell-indent))
 
-(add-hook 'haskell-mode-hook 'lest/haskell-mode-hook)
+(add-hook 'haskell-mode-hook 'my-haskell-mode-hook)
 
 (add-to-list 'exec-path "~/.cabal/bin")
